e9afe024d4316b5341be9f4cc972532a77d0976b
Sawyer Borror <sawyerb@ksu.edu>
2024-11-14 14:04:34 -0600

    Add menu redirect user login to CAS

--- codebase/web/modules/custom/ksul_view_changes/src/Routing/RouteSubscriber.php
+++ codebase/web/modules/custom/ksul_view_changes/src/Routing/RouteSubscriber.php
@@ -8,6 +8,21 @@ use Symfony\Component\Routing\RouteCollection;
 class RouteSubscriber extends RouteSubscriberBase {
 
   protected function alterRoutes(RouteCollection $collection) {
+
+    // check for those pesky fgsc duplicate title records and clean the url_alias
+    $pattern = '/(fgsc-[a-zA-Z0-9]+)-[0-9]$/';
+    foreach ($collection as $route_name => $route) {
+      $path = $route->getPath();
+
+      // Check if the path matches the pattern
+      if (preg_match($pattern, $path)) {
+        // Remove the trailing '-0' or '-[number]' if it matches the pattern
+        $new_path = preg_replace($pattern, '$1', $path);
+
+        // Set the updated path
+        $route->setPath($new_path);
+      }
+    }
     // Override the login path to point to /caslogin.
     if ($route = $collection->get('user.login')) {
       $route->setPath('/caslogin');
